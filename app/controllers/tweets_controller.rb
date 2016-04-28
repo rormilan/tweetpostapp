1254134402694c905a2d9323ef2f42ec01dd828c
Milan Pudasini <esror.milan@gmail.com>
2016-04-28 11:56:24 +0545

    adds new and create methods and views with the routes defined

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,14 @@
 class TweetsController < ApplicationController
+  def new
+
+  end
+  def create
+    update_status(twitter_params[:message])
+    flash[:success] = @response
+    redirect_to root_path
+  end
+  private
+  def twitter_params
+    params.require(:tweet).permit(:message)
+  end
 end
